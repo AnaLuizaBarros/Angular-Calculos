c25774fbe2576c356007d363e485d9ece2250420
Menialinda <analuizagbarros@gmail.com>
2020-05-18 14:10:02 -0300

    Navegação

---
+++ src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+
+
+@Component({
+  selector: 'bha-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  animations: [
+    trigger('hover',[
+      state('open', style({
+        opacity: 1,
+        top: '100%',
+        transform: 'rotateX(0deg)',
+        display: 'block'
+      })),
+      state('close', style({
+        display: 'block',
+         opacity: 0
+      })),
+      transition('close => open', [
+        animate('0.1s')
+      ]),
+      transition('open => close', [
+        animate('0.3s')
+      ]),
+    ]),
+  ],
+
+})
+export class NavBarComponent implements OnInit {
+
+ currentState =  'close';
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+ChangeState(){
+  this.currentState = this.currentState === 'open' ? 'close' : 'open';
+}
+}
